e870d2667d27e947677ac16e7591e42c8fca2c6d
gabez1 <gzermeno1@gmail.com>
2022-09-26 14:29:53 -0700

    feat: registerform component

--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -3,24 +3,56 @@ import React from 'react';
 class RegisterForm extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {value: ''};
+      this.state = {name:'', password:''};
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
   
-    handleChange(event) {    this.setState({value: event.target.value});  }
+    handleChange(event) {    
+      const target = event.target;
+      const value = target.value;
+      const name = target.name; 
+      this.setState({
+        [name]: value    
+      });
+    }
+
     handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
+      const { name, password } = this.state
+      alert('A name and password was submitted: ' + this.state.name + " " + this.state.password);
       event.preventDefault();
     }
   
     render() {
       return (
-        <form onSubmit={this.handleSubmit}>        <label>
-            Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-            </label>
-          <input type="submit" value="Submit" />
+        <form onSubmit={this.handleSubmit}
+            style={{
+              position: 'absolute', left: '50%', top: '50%',
+              transform: 'translate(-50%, -50%)'
+          }}>
+          <div>
+            <label htmlFor='name'>Name</label>
+            <br />
+            <input 
+              placeholder='Name'
+              type="text"
+              value={this.state.value} 
+              onChange={this.handleChange} 
+            />
+          </div>
+          <div>
+            <label htmlFor='password'>Password</label>
+            <br />
+            <input 
+              placeholder='Password'
+              type="text"
+              value={this.state.value} 
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <button>Register account</button>
+          </div>
         </form>
       );
     }
