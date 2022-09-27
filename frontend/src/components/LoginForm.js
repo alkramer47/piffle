cc8089db05eb749bb21c323cf99e12a608e5c488
Garrett Chavez <chavezgarrett7@gmail.com>
2022-09-27 15:00:38 -0700

    Finished Home/Login Page w/CSS

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import '../routes/Home.css';
 
 function LoginForm({Login, error}){
     const[details, setDetails] = useState({name:"", email:"", password:""});
@@ -12,28 +13,26 @@ function LoginForm({Login, error}){
     return (
         <div>
             <form onSubmit={submitHandler}>
-                <div className="form-inner">
-                    <h2>Login</h2>
-                    {
-
-                    }
-                    <div className="form-group"> 
-                        <label htmlFor="name">Name:</label>
-                        <input type="text" name="name" id="name"/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" name="email" id="email"/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password:</label>
-                        <input type="password" name="password" id="password"/>
-                    </div>
-                    <input type="submit" value="Login"/>
-                    <input type="submit" value="Sign up"/>
+                    <div className="form-inner">
+                        <h2>Login</h2>
+                            {(error != "") ? (<div className="error">{error}</div>) : ""}
+                        <div className="form-group"> 
+                            <label htmlFor="name">Name:</label>
+                            <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email:</label>
+                            <input type="email" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password:</label>
+                            <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
+                        </div>
+                        <input type="submit" value="Login"/>
+                        <input type="submit" value="Sign up"/>
                 </div>
             </form>
-      </div>
+        </div>
     );
 }
 
--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,6 +1,9 @@
 import React, {useState} from "react";
 import LoginForm from '../components/LoginForm';
+import '../routes/Home.css'
+
 const Home = () => {
+    /* Test user to see successful login */
     const adminUser = {
         email: "admin@admin.com",
         password: "admin123"
@@ -11,18 +14,31 @@ const Home = () => {
 
     const Login = details => {
         console.log(details);
+
+        /* This is where we will handle User information with DB */
+        if(details.email == adminUser.email && details.password == adminUser.password){
+            console.log("Logged in");
+            setUser({
+               name: details.name,
+               email: details.email 
+            });
+        } else {
+            console.log("Incorrect Username and/or Password");
+            setError("Incorrect Username and/or Password")
+        }
     }
 
     const Logout = () => {
         console.log("Logout");
+        setUser({name: "", email: ""});
     }
 
     return (
-        <div className="App">
+        <div className="Home">
             {(user.email != "") ? (
                 <div className="welcome">
                     <h2>Welcome, <span>{user.name}</span></h2>
-                    <button>Logout</button>
+                    <button onClick={Logout}>Logout</button>
                 </div>
             ): (
                <LoginForm Login={Login} error={error}/> 
