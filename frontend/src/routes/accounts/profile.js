b34822ed2f40d223cf133c69892dd0a0eed4ef65
alkramer47 <alkramer12@gmail.com>
2022-10-05 10:51:59 -0700

    made profile with profile pic, edit profile button, friends list

--- frontend/src/routes/accounts/profile.js
+++ frontend/src/routes/accounts/profile.js
@@ -1,13 +1,40 @@
-import React from "react";
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { Component } from "react";
+import Dropdown from 'react-bootstrap/Dropdown';
+import "../../App.css"
 
 const Profile = () => {
-
     return (
         <div>
-            PROFILE TEMPLATE
+            <div class="float-end">
+            <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Edit Profile
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Change profile picture</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Change background</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Change password</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+           
+           </div>
+           <div class="item">
+            <img class="circular_image" src={require('../../assets/puffle_purple.png')} />
+            <span class = "caption"> Profile name </span>
+           </div>
+
+           
+
+           <h1>Friends list: </h1>
+
+           
+           <button class="button">Add Friend</button>
         </div>
+
+        
+
+
     );
 }
 
