deb2411a4bd539664d87ffd7cb0f46170c408474
alkramer47 <alkramer12@gmail.com>
2022-10-12 13:58:18 -0700

    profile styling

--- frontend/src/routes/accounts/profile.js
+++ frontend/src/routes/accounts/profile.js
@@ -1,35 +1,46 @@
 import React, { Component } from "react";
 import Dropdown from 'react-bootstrap/Dropdown';
+import { useNavigate } from "react-router-dom";
 import "../../App.css"
 
 const Profile = () => {
+    const navigate = useNavigate();
+
     return (
         <div>
             <div class="float-end">
-            <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                    Edit Profile
-                </Dropdown.Toggle>
+                <button class="button" style={{backgroundColor: "black"}} onClick={() => navigate('/Conversations')}>
+                    Conversations</button>
+                <Dropdown>
+                    <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+                        Edit Profile
+                    </Dropdown.Toggle>
 
-                <Dropdown.Menu>
-                    <Dropdown.Item href="#/action-1">Change profile picture</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Change background</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Change password</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-           
-           </div>
+                    <Dropdown.Menu variant="dark">
+                        <Dropdown.Item href="#/action-1">Change profile picture</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Change background</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Change password</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
+
            <div class="item">
-            <img class="circular_image" src={require('../../assets/puffle_purple.png')} />
-            <span class = "caption"> Profile name </span>
+                <img class="circular_image" src={require('../../assets/puffle_purple.png')} />
+                <span class = "caption"> Profile name </span>
            </div>
-
-           
 
            <h1>Friends list: </h1>
-
+           <p>
+           <img class="small_circular_image"  style={{backgroundColor: "green"}}src={require('../../assets/puffle_purple.png')} />
+             obama</p>
+           <p>
+             <img class="small_circular_image" src={require('../../assets/puffle_sleep.png')} />
+             ghandi</p>
+           <p>
+           <img class="small_circular_image" src={require('../../assets/puffle_orange.png')} />
+            turing</p> 
+           <button class="button" onClick="onButtonClick()">Add Friend</button>
            
-           <button class="button">Add Friend</button>
         </div>
 
         
@@ -38,4 +49,9 @@ const Profile = () => {
     );
 }
 
+function onButtonClick(){
+    document.getElementById('textInput').className="show";
+  }
+
+  
 export default Profile;
