02173f3582054aa52576144d4f23f05313b7ca07
alkramer47 <alkramer12@gmail.com>
2022-10-25 12:31:34 -0700

    added background images n made changebackground

--- frontend/src/routes/accounts/UpdateBackground.js
+++ frontend/src/routes/accounts/UpdateBackground.js
@@ -3,8 +3,29 @@ import React from "react";
 const UpdateBackground = () => {
     return (
         <div>
-            TEMPLATE
+            Choose a color to be your new profile background
+            <p></p>
+           <button onClick={changeBackground}><img class="small_circular_image"  style={{}}src={require('../../assets/black.png')} alt="black" /></button>
+
+           <button onClick={changeBackground}><img class="small_circular_image"  style={{}}src={require('../../assets/blue.png')} alt="blue" /></button>
+
+           <button onClick={changeBackground}><img class="small_circular_image"  style={{}}src={require('../../assets/red.png')} alt="red" /></button>
+
+           <button onClick={changeBackground}><img class="small_circular_image"  style={{}}src={require('../../assets/orange.png')} alt="orange" /></button>
+
+           <button onClick={changeBackground}><img class="small_circular_image"  style={{}}src={require('../../assets/yellow.png')} alt="yellow" /></button>
+
+           <button onClick={changeBackground}><img class="small_circular_image"  style={{}}src={require('../../assets/purple.png')} alt="purple" /></button>
+
+           <button onClick={changeBackground}><img class="small_circular_image"  style={{}}src={require('../../assets/green.png')} alt="green" /></button>
+
         </div>
+    );
+}
+
+function changeBackground() {
+    return (
+        console.log("clicked")
     );
 }
 
