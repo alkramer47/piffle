acf0e7d79c61baf6d66c8696f569378c1bdde3cc
alkramer47 <alkramer12@gmail.com>
2022-10-25 12:23:27 -0700

    updatepicture page working

--- frontend/src/routes/accounts/UpdatePicture.js
+++ frontend/src/routes/accounts/UpdatePicture.js
@@ -6,11 +6,30 @@ const UpdatePicture = () => {
     
     return (
         <div>
-            TEMP UPDATE PIC
-            TEMP TEMP TEMP
-            SHOW SOMETHING DAMN IT
+            Choose a picture to be your new profile picture
+            <p></p>
+           <button onClick={changePicture}><img class="small_circular_image"  style={{}}src={require('../../assets/puffle_blue.png')} alt="purple_puffle" /></button>
+
+           <button onClick={changePicture}><img class="small_circular_image"  style={{}}src={require('../../assets/puffle_brown.png')} alt="purple_puffle" /></button>
+
+           <button onClick={changePicture}><img class="small_circular_image"  style={{}}src={require('../../assets/puffle_orange.png')} alt="purple_puffle" /></button>
+
+           <button onClick={changePicture}><img class="small_circular_image"  style={{}}src={require('../../assets/puffle_pink.png')} alt="purple_puffle" /></button>
+
+           <button onClick={changePicture}><img class="small_circular_image"  style={{}}src={require('../../assets/puffle_purple.png')} alt="purple_puffle" /></button>
+
+           <button onClick={changePicture}><img class="small_circular_image"  style={{}}src={require('../../assets/puffle_sleep.png')} alt="purple_puffle" /></button>
+
         </div>
     );
 }
+
+function changePicture(){
+    return (
+        console.log("clicked")
+    );
+}
+
+
 
 export default UpdatePicture;
