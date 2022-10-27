5ee39ba04c96bf3bd84c64fb9993dc1470548bc8
Nathan Taylor <nathan.taylor562@gmail.com>
2022-10-27 10:32:55 -0700

    Added firebase hosting

---
+++ frontend/src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD1_dlIRMw06afHJeX3i_zSNZmCrFtKKfY",
+  authDomain: "piffle-ded6f.firebaseapp.com",
+  projectId: "piffle-ded6f",
+  storageBucket: "piffle-ded6f.appspot.com",
+  messagingSenderId: "1031090005309",
+  appId: "1:1031090005309:web:be1b172ae24fdb6df25154",
+  measurementId: "G-0ED80VJZJX"
+};
+
+export default firebaseConfig;
